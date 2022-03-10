4b8232878e9a6d3dcfdb852db18dc834ceba614b
Saauren Mankad <sman0039@student.monash.edu>
2022-03-10 14:29:56 +1100

    Initial Commit

---
+++ FIT3178-W02-Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIT3178-W02-Lab
+//
+//  Created by Saauren Mankad on 10/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
